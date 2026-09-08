31ab65326588b14cbab0b399befa416446405ebc
Leocqrl <2810leo768@gmail.com>
2026-09-08 16:42:46 +0200

    ajout variables et Contextes

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,8 +1,19 @@
 def puissance (a, b):
 	is_int(a)
 	is_int(b)
-	return (a**b)
+	result = 1
+	if b<0:
+		for i in range(-b):
+			result *= a 
+		if a == 0 :
+			raise ValueError("0 à une puissance négative est impossible")		
+		else : return (1/result)
+
+	for i in range(b):
+		result *= a
+	return (result)
 
 def is_int (var):
-	if not type(var) is int:
+	if type(var) != int:
 		raise TypeError("Only integers are allowed")
+
